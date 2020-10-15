9e9ea7b7394d5a5dd0f966730cbd947067cab814
melisekm <46221266+melisekm@users.noreply.github.com>
2020-10-15 23:23:06 +0200

    hlavna struktura

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import heapq as hq  # https://docs.python.org/2/library/heapq.html
 
 
 class Node:
-    def __init__(self, stav, parent, lastOperator, heuristika):
+    def __init__(self, stav, parent, lastOperator, heuristika, ciel):
         self.stav = stav  # int[]
         self.parent = parent  # class Node
-        # string, ked sa pozrem na rodica mam vediet vypocitat cestu
+        # string, ked sa pozrem na rodica mam vediet vypocitat cestu mozem usetrit
         self.lastOperator = lastOperator
-        self.cenaCiel = heuristika(stav)  # podla heurestickej funkcie
+        self.cenaCiel = heuristika(stav, ciel)  # podla heurestickej funkcie
 
-    def __lt__(self, other):  # custom comparator
+    def __lt__(self, other):  # custom comparator pre minHeap
         return self.cenaCiel < other.cenaCiel
 
 
@@ -24,15 +24,23 @@ class MinHeap:
         return hq.heappop(self.heap)
 
 
-def najdiMedzeru(stav):
+def ziskajPozicieCisel(ciel):
+    global pozicieCisel
+    pozicieCisel = {}
+    for sublist in ciel:
+        for cislo in sublist:
+            pozicieCisel[cislo] = najdiPosCisla(ciel, cislo)
+
+
+def najdiPosCisla(stav, target):
     for i, sublist in enumerate(stav):
         for j, cislo in enumerate(sublist):
-            if cislo == 0:
+            if cislo == target:
                 return (i, j)
 
 
 def vykonajOperator(stav, operator):
-    i, j = najdiMedzeru(stav)
+    i, j = najdiPosCisla(stav, 0)  # pozicia medzery
     novyStav = [sublist[:] for sublist in stav]  # skopiruje data
 
     if operator == "VPRAVO":
@@ -77,7 +85,7 @@ def loadFile(nazov):
         for index, line in enumerate(file):
             stav.append([])
             znaky = line.strip().split(" ")
-            stav[index].append(znaky)
+            stav[index] = list(map(int, znaky))
     return stav
 
 
@@ -85,11 +93,11 @@ def loadInput():
     print("Pocet policok, ktore nie su na svojom mieste[1]")
     print("Súčet vzdialeností jednotlivých políčok od ich cieľovej pozície")
     # heuristika = int(input("Zadajte typ heuristiky: "))
-    heuristika = 1
+    heuristika = 2
     if heuristika == 1:
         heuristika = heuristika1
     elif heuristika == 2:
-        heurestika = heuristika2
+        heuristika = heuristika2
     else:
         print("Zly vstup :)")
         quit()
@@ -98,27 +106,41 @@ def loadInput():
     return (start, ciel), heuristika
 
 
-def heuristika1(stav):
-    return 1
-    pass
+def heuristika1(stav, ciel):
+    vysledok = 0
+    for i, sublist in enumerate(stav):
+        for j, cislo in enumerate(sublist):
+            if cislo != 0 and cislo != ciel[i][j]:
+                vysledok += 1
+
+    return vysledok
 
 
-def heuristika2(stav):
-    print("pes")
-    pass
+# je mozne si predvypocitat pozicie pre kazde cislo
+def heuristika2(stav, ciel):
+    vysledok = 0
+    for i, sublist in enumerate(stav):
+        for j, cislo in enumerate(sublist):
+            if cislo != 0:
+                # x, y = najdiPosCisla(ciel, cislo)
+                x, y = pozicieCisel[cislo]
+                vysledok += abs((x - i)) + abs((y - j))
+
+    return vysledok
 
 
 def lacne_hladanie(problem, heuristika):
     start, ciel = problem
-    start = Node(start, None, None, heuristika)
-    ciel = Node(ciel, None, None, heuristika)
+    if "heuristika2" == heuristika.__name__:
+        ziskajPozicieCisel(ciel)
+    start = Node(start, None, None, heuristika, ciel)
+    ciel = Node(ciel, None, None, heuristika, ciel)
     spracovaneStavy = {}
     minHeap = MinHeap(start)
 
 
 if __name__ == "__main__":
     problem, heuristika = loadInput()
-    # riesenie = lacne_hladanie(problem, heuristika)
-    vstup = [[6, 1, 2, 3], [4, 5, 7, 0], [8, 9, 10, 11]]
-    vystup = DOLE(vstup)
+    riesenie = lacne_hladanie(problem, heuristika)
+    # vystup = heuristika2(problem[0], problem[1])
     pass
