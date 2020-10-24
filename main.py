af5d8a5bcbed4962daa097c7a504efea885c712b
melisekm <46221266+melisekm@users.noreply.github.com>
2020-10-24 18:03:43 +0200

    nove veci

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import heapq as hq  # https://docs.python.org/2/library/heapq.html
 import timeit
 
-import drawImages as draw
-
 # uzol
 class Node:
     def __init__(self, stav, parent, lastOperator, heuristika, ciel):
@@ -15,13 +13,10 @@ class Node:
     def __lt__(self, other):  # custom comparator pre minHeap
         return self.cenaCiel < other.cenaCiel
 
-    def __le__(self, other):
-        return self.cenaCiel <= other.cenaCiel
-
 
 # wrapper pre reprezentaciu minhaldy
 class MinHeap:
-    def __init__(self, start):
+    def __init__(self, start):  # vytvori jednoprvkovu haldu
         self.heap = [start]
 
     # vlozi prvok do haldy
@@ -47,19 +42,46 @@ def loadFile(nazov):
     return stav
 
 
+def getKonfiguracia(stav):
+    result = "\n"
+    for riadok in stav:
+        for cislo in riadok:
+            result += str(cislo) + " "
+        result += "\n"
+    return result
+
+
 def loadInput():
+    print("Koniec [q]")
+    print("Predvygenerovane: [3x2] [3x3] [5x2] [4x3] [2x7] [5x5] [9x3] [6x6] [7x7]")
+    print("alebo [vlastny] zo suboru tests - start.txt a ciel.txt: ")
+    vstup = input()
+
+    if vstup in ("3x2", "3x3", "5x2", "4x3", "2x7", "6x6", "7x7", "9x3"):
+        path = "tests\\" + vstup + "\\"
+    elif vstup == "vlastny":
+        path = "tests\\"
+    elif vstup == "q":
+        print("Ukoncujem")
+        quit()
+    else:
+        return None, None
+
+    start = loadFile(path + "start.txt")
+    ciel = loadFile(path + "ciel.txt")
+    print("Start:" + getKonfiguracia(start))
+    print("Ciel:" + getKonfiguracia(ciel))
+    print("Zadajte typ heuristiky: ")
     print("Pocet policok, ktore nie su na svojom mieste[1]")
     print("Súčet vzdialeností jednotlivých políčok od ich cieľovej pozície[2]")
-    heuristika = int(input("Zadajte typ heuristiky: "))  # vyber funkcie pre heuristiku
-    if heuristika == 1:
+    heuristika = input()  # vyber funkcie pre heuristiku
+    if heuristika == "1":
         heuristika = heuristika1
-    elif heuristika == 2:
+    elif heuristika == "2":
         heuristika = heuristika2
     else:
-        print("Zly vstup :)")
-        quit()
-    start = loadFile("start.txt")
-    ciel = loadFile("ciel.txt")
+        return None, None
+    print(f"Spustam hladanie ({len(start)}x{len(start[0])})...")
     return (start, ciel), heuristika
 
 
@@ -101,7 +123,7 @@ def heuristika1(stav, ciel):
     vysledok = 0
     for i, sublist in enumerate(stav):
         for j, cislo in enumerate(sublist):
-            if cislo != 0 and cislo != ciel[i][j]:  # kontrolujem ci tam je alebo nie
+            if cislo != 0 and cislo != ciel[i][j]:
                 vysledok += 1
 
     return vysledok
@@ -122,21 +144,21 @@ def heuristika2(stav, ciel):
     for i, sublist in enumerate(stav):
         for j, cislo in enumerate(sublist):
             if cislo != 0:
-                # x, y = najdiPosCisla(ciel, cislo)  # pocitam
                 x, y = pozicieCisel[cislo]  # predvypocitana pozicia cisla v ciely
                 vysledok += abs((x - i)) + abs((y - j))
 
     return vysledok
 
 
-# ad hoc :D
-def opacnySmer(newOperatorIndex, parentOperator, operatory):
-    if parentOperator is not None:
-        parentOperatorIndex = operatory.index(parentOperator)
-        if newOperatorIndex == parentOperatorIndex:
-            return False
-        if (newOperatorIndex + parentOperatorIndex) % 2 == 0:
-            return True
+def opacnySmer(newOperator, parentOperator):
+    if newOperator == "VPRAVO" and parentOperator == "VLAVO":
+        return True
+    elif newOperator == "VLAVO" and parentOperator == "VPRAVO":
+        return True
+    elif newOperator == "HORE" and parentOperator == "DOLE":
+        return True
+    elif newOperator == "DOLE" and parentOperator == "HORE":
+        return True
     return False
 
 
@@ -151,8 +173,8 @@ def vytriedNasledovnikov(nasledovnici, spracovaneStavy, minHeap):
 
 def vytvorNasledovnikov(parent, heuristika, ciel, operatory):
     nasledovnici = []
-    for i, operator in enumerate(operatory):
-        if not opacnySmer(i, parent.lastOperator, operatory):
+    for operator in operatory:
+        if not opacnySmer(operator, parent.lastOperator):
             novyStav = vykonajOperator(parent.stav, operator)
             if novyStav is not None:
                 novyUzol = Node(novyStav, parent, operator, heuristika, ciel)
@@ -162,86 +184,60 @@ def vytvorNasledovnikov(parent, heuristika, ciel, operatory):
 
 def zostavRiesenie(uzol):
     result = []
-    uzly = []
     while uzol is not None:
         result.append(uzol.lastOperator)
-        uzly.append(uzol)
         uzol = uzol.parent
     result.reverse()
-    uzly.reverse()
-    return result[1:], uzly  # bez None od start pozicie
+    return result[1:]  # bez None od start pozicie
 
 
 def lacne_hladanie(problem, heuristika):
+    riesenie = None
     start, ciel = problem
     if heuristika.__name__ == "heuristika2":
         ziskajPozicieCisel(ciel)  # ak je to heurestika 2 dokazem si dopredu vypocitat pozicie
     operatory = ["VPRAVO", "DOLE", "VLAVO", "HORE"]
     start = Node(start, None, None, heuristika, ciel)
-    spracovaneStavy = {}  # hashTable uz preskumanych stavov
+    spracovaneStavy = set()  # hashSet uz preskumanych stavov
     minHeap = MinHeap(start)  # vlozi start uzol do haldy
+
     while not minHeap.isEmpty():  # pokym existuju vytvorene ale nespracovane uzly
         current = minHeap.pop()  # vyberiem z topu haldy
+        hashableStav = tuple(tuple(riadok) for riadok in current.stav)  # konvert na hashable
+
         if current.stav == ciel:  # nasiel som koniec
-            print(f"Pocet spracovanych uzlov: {len(spracovaneStavy)}")
-            print(f"Pocet vygenerovanych uzlov: {len(spracovaneStavy) + len(minHeap.heap)}")
-            return zostavRiesenie(current)
+            riesenie = zostavRiesenie(current)
+            break
+
         nasledovnici = vytvorNasledovnikov(current, heuristika, ciel, operatory)  # potencialny
-        hashableStav = tuple(tuple(riadok) for riadok in current.stav)  # konvert na hashable TODO
-        spracovaneStavy[hashableStav] = current
+        spracovaneStavy.add(hashableStav)
         vytriedNasledovnikov(nasledovnici, spracovaneStavy, minHeap)
         # odstranenie uz spracovanych stavov
-    return None
 
-
-def pocetVacsichPredchodcov(start, cislo):
-    res = 0
-    for sublist in start:
-        for num in sublist:
-            if num != 0:
-                if num == cislo:
-                    return res
-                if num > cislo:
-                    res += 1
-    return res
-
-
-def paritaPreStav(stav):
-    res = 0
-    for i, sublist in enumerate(stav, start=1):
-        for cislo in sublist:
-            if cislo != 0:
-                res += pocetVacsichPredchodcov(stav, cislo)
-            else:
-                medzeraPos = i
-    return res, medzeraPos
-
-
-def isSolvable(problem):
-    start, ciel = problem
-    parita1, medzera1 = paritaPreStav(start)
-    if len(start) % 2 == 1 and len(start[0]) % 2 == 1:
-        if parita1 % 2 == 1:  # 7x7 toto nie je zjavne na vsetko :)
-            return False
-        else:
-            return True
-    parita2, medzera2 = paritaPreStav(ciel)
-    if (parita1 + medzera1) % 2 == (parita2 + medzera2) % 2:
-        return True
-    return False
+    print(f"Pocet spracovanych uzlov: {len(spracovaneStavy)}")
+    print(f"Pocet vygenerovanych uzlov: {len(spracovaneStavy) + len(minHeap.heap)}")
+    return riesenie
 
 
 if __name__ == "__main__":
-    problem, heuristika = loadInput()
-    if isSolvable(problem):
-        start = timeit.default_timer()
-        riesenie, uzly = lacne_hladanie(problem, heuristika)
-        end = timeit.default_timer() - start
-        for i in riesenie:
-            print(i)
-        print(f"Hladanie trvalo: {end}")
-        print(f"Dlzka riesenia: {len(riesenie)}")
-        draw.drawImages(uzly)
+    while True:
+        problem, heuristika = loadInput()
+        if problem == None:
+            print("Zly vstup :)")
+            continue
 
-    else:
-        print("Riesenie neexistuje.")
+        start = timeit.default_timer()
+
+        riesenie = lacne_hladanie(problem, heuristika)
+
+        end = timeit.default_timer() - start
+        print(f"Hladanie trvalo: {end}")
+        if riesenie is not None:
+            print(f"Dlzka riesenia: {len(riesenie)}")
+            vstup = input("Chcete vypisat riesenie?: ")
+            if vstup == "y":
+                if riesenie is not None:
+                    for i in riesenie:
+                        print(i)
+        else:
+            print("Riesenie neexistuje.")
